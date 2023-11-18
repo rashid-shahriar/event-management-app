83adabaad3aa1aea9369f9d958e94b7d7f878d99
rashid-shahriar <rashidslink@gmail.com>
2023-11-19 00:48:30 +0600

    backend auth

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,48 @@
-// backend/index.js
+const http = require("http");
+const app = require("./app");
 
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3001; // Set the port
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
 
-app.get("/", (req, res) => {
-  res.send("Server is running"); // A simple route for testing
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on("error", errorHandler);
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+server.listen(port);
